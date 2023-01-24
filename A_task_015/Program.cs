3278c2b94593ae9a399f1ec878dc81d163603693
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-01-24 21:25:32 +0800

    Программа для задачи 15 завершена!

--- A_task_015/Program.cs
+++ A_task_015/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 15: Напишите программу, которая принимает 
+на вход цифру, обозначающую день недели, и 
+проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите № дня недели:  ");
+
+string input_number = Console.ReadLine();
+int a_number_of_day = Convert.ToInt32(input_number);
+
+Console.Write("Будет ли он выходным? ");
+
+if (1 <= a_number_of_day && a_number_of_day <= 5)
+
+{
+    Console.Write("Нет!");
+}
+
+else if (6 <= a_number_of_day && a_number_of_day <= 7)
+
+{
+    Console.Write("Да!");
+}
+
+else
+
+{
+    Console.Write("Это число, вообще, не может быть днём недели!");
+}
+
