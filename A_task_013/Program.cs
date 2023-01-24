2d66e3175af62484800d7d176ba06baf8db3406b
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-01-24 20:21:31 +0800

    Программа для задачи 13 написанна!

--- A_task_013/Program.cs
+++ A_task_013/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит 
+третью цифру заданного числа или сообщает, что 
+третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int GetNumber(int a_count)
+{
+    int result = a_count/100-(a_count/1000)*10;
+    return result;
+}
+
+int number = GetNumber(1);
+
+if (number == 0) 
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else {
+    Console.WriteLine(number);
+}
