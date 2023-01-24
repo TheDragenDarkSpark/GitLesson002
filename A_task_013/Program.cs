3909099b319f3f38f780772177ec87db730b00f0
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-01-24 20:41:39 +0800

    Программа для задачи 13 завершена!

--- A_task_013/Program.cs
+++ A_task_013/Program.cs
@@ -7,19 +7,26 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
-
+Console.Clear();
 int GetNumber(int a_count)
 {
     int result = a_count/100-(a_count/1000)*10;
     return result;
 }
 
-int number = GetNumber(1);
+Console.WriteLine("Введите любое число:");
+string input_number = Console.ReadLine();
+int transit_number = Convert.ToInt32(input_number);
+int number = GetNumber(transit_number);
 
 if (number == 0) 
 {
     Console.WriteLine("третьей цифры нет");
 }
 else {
-    Console.WriteLine(number);
+    Console.WriteLine($"Вывод третьей цифры числа {transit_number} - это: {number}");
 }
+
+
+
+
