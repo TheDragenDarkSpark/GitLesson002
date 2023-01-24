5bb555d5573324ccc161866bc7465cc37a0fef04
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-01-24 19:45:54 +0800

    Составленна программа для задачи №10.

--- A_task_010/Program.cs
+++ A_task_010/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая 
+принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого 
+числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+
+int GetNumber(int a_count)
+{
+    int second_number = a_count/10-(a_count/100)*10;
+    return second_number;
+}
+
+Console.WriteLine("Введите число: ");
+string first_data = Console.ReadLine();
+
+int number = Convert.ToInt32(first_data);
+
+Console.WriteLine(GetNumber(number));
+
